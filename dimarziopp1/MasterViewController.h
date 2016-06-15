0b71a53ae4323a4a74ab05c261d005922bdf1dd8
Harrison Ching <hching@myhost.local>
2016-06-15 15:54:59 -0700

    initial commit

---
+++ dimarziopp1/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  dimarziopp1
+//
+//  Created by Harrison Ching on 6/15/16.
+//  Copyright © 2016 Harrison Ching. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
