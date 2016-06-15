4cb64a3e306f2ab2842934236b04050be92ec4a1
Harrison Ching <hching@myhost.local>
2016-06-15 16:08:07 -0700

    rename files

---
+++ dimarziopp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dimarziopp1
+//
+//  Created by Harrison Ching on 6/15/16.
+//  Copyright © 2016 Harrison Ching. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
